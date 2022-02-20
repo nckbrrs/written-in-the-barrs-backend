ab2bb6e63b97ae3a56bcc11eba703dde71b5ce40
nckbrrs <nckbrrs@icloud.com>
2022-02-20 17:59:36 -0500

    jus some cleaning up

--- invitee-router.js
+++ invitee-router.js
@@ -2,58 +2,64 @@ import { Router } from 'express';
 import inviteeRepository from './invitee-repository.js';
 const inviteeRouter = Router();
 
-// allow CORS for preflight requests to this route
-inviteeRouter.options('/', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.end();
-});
-
-// create
 inviteeRouter.post('/', async (req, res) => {
-    const invitee = inviteeRepository.createEntity(req.body);
-    const id = await inviteeRepository.save(invitee);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json({ entityId: id });
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    try {
+        const invitee = inviteeRepository.createEntity(req.body);
+        const id = await inviteeRepository.save(invitee);
+        res.json({ entityId: id });
+    } catch (err) {
+        res.json({ result: 'ERROR! ' + err.message});
+    }
 });
 
-// search
 inviteeRouter.get('/', async (req, res) => {
-    const firstName = req.query.first;
-    const lastName = req.query.last;
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    const firstAndLastMatches = await inviteeRepository.search()
-        .where('firstName').equals(firstName)
-        .and('lastName').equals(lastName)
-        .return.all();
+    try {
+        const firstName = req.query.first;
+        const lastName = req.query.last;
 
-    const justLastMatches = await inviteeRepository.search()
-        .where('lastName').equals(lastName)
-        .return.all();
+        const firstAndLastMatches = await inviteeRepository.search()
+            .where('firstName').equals(firstName)
+            .and('lastName').equals(lastName)
+            .return.all();
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json(firstAndLastMatches.length > 0 ? firstAndLastMatches : justLastMatches);
+        const justLastMatches = await inviteeRepository.search()
+            .where('lastName').equals(lastName)
+            .return.all();
+
+        res.json(firstAndLastMatches.length > 0 ? firstAndLastMatches : justLastMatches);
+    } catch (err) {
+        res.json({ result: 'ERROR! ' + err.message});
+    }
 });
 
-// allow CORS for preflight requests to this route
-inviteeRouter.options('/createIndex', function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.end();
-});
-
-// create index
 inviteeRouter.get('/createIndex', async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     try {
         await inviteeRepository.createIndex();
         res.json({ result: 'created new invitee index!' });
     } catch (err) {
-        res.json({ result: `ERROR! ${err.message}`})
+        res.json({ result: 'ERROR! ' + err.message});
     }
+});
+
+
+inviteeRouter.options('/', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.end();
+});
+
+inviteeRouter.options('/createIndex', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.end();
 });
 
 export default inviteeRouter;
