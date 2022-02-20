b7d63fb89bbf7930facb3c4db195a43330e1c682
nckbrrs <nckbrrs@icloud.com>
2022-02-20 17:38:27 -0500

    still workin cors lol

--- invitee-router.js
+++ invitee-router.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import inviteeRepository from './invitee-repository.js';
 const inviteeRouter = Router();
 
+/*
 // allow CORS for preflight requests to this route
 inviteeRouter.options('/', function (req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -9,6 +10,7 @@ inviteeRouter.options('/', function (req, res) {
     res.setHeader("Access-Control-Allow-Headers", "*");
     res.end();
 });
+*/
 
 // create
 inviteeRouter.post('/', async (req, res) => {
@@ -34,6 +36,7 @@ inviteeRouter.get('/', async (req, res) => {
     res.json(firstAndLastMatches.length > 0 ? firstAndLastMatches : justLastMatches);
 });
 
+/*
 // allow CORS for preflight requests to this route
 inviteeRouter.options('/createIndex', function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -41,6 +44,7 @@ inviteeRouter.options('/createIndex', function (req, res) {
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.end();
 });
+*/
 
 // create index
 inviteeRouter.get('/createIndex', async (req, res) => {
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import inviteeRouter from './invitee-router.js';
-const cors = require('cors');
+import cors from 'cors';
 
 let app = new express();
 app.use(express.json());
 app.use('/invitees', inviteeRouter);
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.listen(process.env.PORT || 5000);
