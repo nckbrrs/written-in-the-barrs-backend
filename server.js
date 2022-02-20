adb27136080e2eefe4491233840a2f46b1aea2e0
nckbrrs <nckbrrs@icloud.com>
2022-02-20 17:11:48 -0500

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import inviteeRouter from './invitee-router.js';
+const cors = require('cors');
 
 let app = new express();
 app.use(express.json());
 app.use('/invitees', inviteeRouter);
-app.listen(process.env.PORT || 5000)
+app.use(cors({ origin: '*' }));
+app.listen(process.env.PORT || 5000);
